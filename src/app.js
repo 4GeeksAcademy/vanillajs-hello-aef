0afb905aa4dfe00aa71ed8718d9e1df712c2e4be
Andrés Fernandes <127229387+andresinhof98@users.noreply.github.com>
2024-02-15 04:14:01 +0000

    listoo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function excusa() {
+  let who = ["My grandma", "His turtle", "My bird"];
+  let action = ["peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["right on time", "when I finished", "during my lunch"];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Construct the excuse dynamically, ensuring valid grammar
+  let excuse = `Oh no! ${randomWho} ${randomAction} ${randomWhat} ${randomWhen}. I'm so sorry, I can't make it to class.`;
+
+  // Output the excuse in both the console and on the page
+  console.log(excuse);
+  document.write(excuse); // Write to the HTML as requested
 };
